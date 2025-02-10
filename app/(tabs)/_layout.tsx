8de178ee5a78bf0da60b842116db5e974d8a7f08
Lucas De La Fuente <lucasdlf17@gmail.com>
2025-02-10 16:09:18 -0300

    tabBar styles

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -17,8 +17,17 @@ export default function TabLayout() {
         tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
         tabBarButton: HapticTab,
+        tabBarIconStyle: {
+          flex: 1,
+          top: 5,
+          width: 70,
+          height: 70,
+          backgroundColor: "transparent",
+        },
         tabBarBackground: TabBarBackground,
         tabBarStyle: {
+          alignItems: "center",
+          height: 70,
           backgroundColor: Colors[colorScheme ?? "light"].tabBackground,
           borderTopWidth: 2,
           borderTopColor: Colors[colorScheme ?? "light"].tint,
@@ -28,13 +37,46 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
+          title: "Inicio",
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
+            <IconSymbol size={40} name="house.fill" color={color} />
           ),
         }}
       />
+
       <Tabs.Screen
+        name="addTask"
+        options={{
+          title: "",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={70} name="add-circle-outline" color={color} />
+          ),
+          tabBarItemStyle: {
+            position: "absolute",
+            justifyContent: "center",
+            alignItems: "center",
+            bottom: 20,
+            left: "50%",
+            transform: [{ translateX: -35 }],
+            borderRadius: 100,
+            paddingLeft: 5,
+            paddingRight: 5,
+            backgroundColor: Colors[colorScheme ?? "light"].tabBackground,
+          },
+        }}
+      />
+
+      <Tabs.Screen
+        name="menu"
+        options={{
+          title: "Menú",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={40} name="menubar.rectangle" color={color} />
+          ),
+        }}
+      />
+
+      {/* <Tabs.Screen
         name="explore"
         options={{
           title: "Explore",
@@ -42,7 +84,7 @@ export default function TabLayout() {
             <IconSymbol size={28} name="paperplane.fill" color={color} />
           ),
         }}
-      />
+      /> */}
     </Tabs>
   );
 }
--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -13,6 +13,9 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  "menubar.rectangle" : "menu",
+  "add": "add",
+  "add-circle-outline": "add-circle-outline"
 } as Partial<
   Record<
     import('expo-symbols').SymbolViewProps['name'],
@@ -39,5 +42,5 @@ export function IconSymbol({
   style?: StyleProp<ViewStyle>;
   weight?: SymbolWeight;
 }) {
-  return <MaterialIcons color={color} size={size} name={MAPPING[name]} style={style} />;
+  return <MaterialIcons color={color} size={size} name={MAPPING[name]} styles={style} />;
 }
